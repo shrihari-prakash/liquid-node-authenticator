f3577fa74b401423adc23b55861b8e1d7425bd11
Shrihari Prakash <shrihariprakash@outlook.com>
2024-01-31 22:42:12 +0530

    Added UMD build.

--- rollup.dist.config.js
+++ rollup.dist.config.js
@@ -1,17 +1,10 @@
-import { getBabelOutputPlugin } from '@rollup/plugin-babel'
-
 export default [{
   input: 'lib/index.js',
   output: [
     {
       file: 'dist/index.js',
-      format: 'es',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
+      format: 'umd',
+      name: "liquidNodeAuthenticator",
       sourcemap: true
     }
   ]
